e8035bf8d28cb2f916baa93514c83b5e08ebd070
Akshay <akshusharmagi@gmail.com>
2025-01-05 21:41:46 +0530

    Init DataBase

--- src/app.js
+++ src/app.js
@@ -1,56 +1,30 @@
 const express = require("express");
-const { adminAuth, userAuth } = require("./middlewares/auth");
+const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
 
-app.use("/admin", adminAuth);
-// app.use("/user",userAuth)
-app.get("/admin/getAllData", (req, res) => {
-  res.send("All Data send");
-});
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("User deleted");
-});
-
-app.get("/user", userAuth, (req, res) => {
-  res.send("User Created");
-});
-app.get("/user/userLogin", (req, res) => {
-  res.send("User login sucessfully");
-});
-
-app.listen(3000, () => {
-  console.log("Server is running on the port 3000");
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Akshay",
+    lastName: "Sharma",
+    emailId: "akshay@sharma.com",
+    password: "akshay@001",
+  });
+  try {
+    await user.save();
+    res.send("User Added Sucessfully");
+  } catch (err) {
+    res.status(400).send("Error Saving the user:" + err.message);
+  }
 });
 
-// app.use("/", (req, res) => {
-//   res.send("Add /test or /hello for responce from server ");
-// });
-
-// app.use("/hello", (req, res) => {
-//   res.send("Hello from the server");
-// });
-// app.post("/test", (req, res) => {
-//   res.send("testing done ");
-// });
-// app.get("/test", (req, res) => {
-//   res.send({ firstName: "Akshay", lastName: "Shamra" });
-// });
-// app.use("/nodemon", (req, res) => {
-//   res.send("nodemon is the og server refresher ");
-// });
-// app.use("/test", (req, res, next) => {
-//   res.send("Data deleted");
-//   console.log("1st middleware")
-//   // next();
-// });
-
-// app.use("/test", (req, res, next) => {
-//   // res.send("Data deleted");
-//   console.log("2st middleware")
-//   next();
-// });
-// app.use("/test", (req, res,next) => {
-//   res.send("Data deleted");
-//   // console.log("3st middleware")
-//   next();
-// });
+connectDB()
+  .then(() => {
+    console.log("DataBase connected sucesssfullly");
+    app.listen(7777, () => {
+      console.log("Server is running on the port 3000");
+    });
+  })
+  .catch((err) => {
+    console.log("DataBase not connected");
+  });
