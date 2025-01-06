5ba009d4903af9c76cb801bdd8868f199c1a5329
Akshay <akshusharmagi@gmail.com>
2025-01-06 15:17:32 +0530

    Making of the API's

--- src/app.js
+++ src/app.js
@@ -2,8 +2,9 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
-app.use(express.json());
 
+// Signup API for the user
+app.use(express.json());
 app.post("/signup", async (req, res) => {
   const user = new User(req.body);
   try {
@@ -14,6 +15,51 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+// get user by email id
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  //   const users = await User.findOne({ emailId: userEmail });
+  //   res.send(users);
+  // });
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.lenght === 0) {
+      res.status(404).send("USer not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(404).send("USer not found");
+  }
+});
+
+// Feed API - get / feed - get all the user from the database
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(404).send("USer not found");
+  }
+});
+
+// Delete a user API
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deletted Sucessfully");
+  } catch (err) {
+    res.status(404).send("USer not found");
+  }
+});
+
+// API for update the data of the user
+
+
+// Connect to the database
 connectDB()
   .then(() => {
     console.log("DataBase connected sucesssfullly");
